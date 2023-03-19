a66c300d3eefdbfbb1a4a6504fb4a648c4cfad53
Mooseman <vikasmk@gmail.com>
2023-03-19 12:40:59 +0530

    Added ground scene

--- Peppa/Peppa_game/peppa.gd
+++ Peppa/Peppa_game/peppa.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 @export var speed = 50
 var screen_size # Size of the game window
+var let_gravity_work = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
@@ -14,11 +15,13 @@ func _process(delta):
 		velocity.x -= 1 
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1 
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-	
+	if let_gravity_work == false:
+		if Input.is_action_pressed("move_up"):
+			velocity.y -= 1
+		if Input.is_action_pressed("move_down"):
+			velocity.y += 1
+	else:
+		velocity.y = velocity.y + 0.5 * gravity * delta 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite2D.play()
